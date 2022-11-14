9ac03f37be7e5561bb312ff40bc5bb53a6e1374a
Matthew Earnest <mjearnest@crimson.ua.edu>
2022-11-14 13:39:30 -0600

    api fix

--- api/Controllers/ordersController.cs
+++ api/Controllers/ordersController.cs
@@ -19,7 +19,7 @@ namespace api.Controllers
         }
 
         // GET: api/orders/5
-        [HttpGet("{id}", Name = "Get")]
+        [HttpGet("{id}", Name = "Getit")]
         public string Get(int id)
         {
             return "value";
