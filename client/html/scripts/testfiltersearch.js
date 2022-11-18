0a939cca61b816b66d8323ba0a077b73640ebb98
Bella Kimbrell <bellakimbrell@Bellas-MacBook-Pro.local>
2022-11-18 15:23:06 -0600

    filter

--- client/html/scripts/testfiltersearch.js
+++ client/html/scripts/testfiltersearch.js
@@ -133,3 +133,100 @@ function getDate(){
 
 
 
+
+
+
+
+
+
+
+
+
+//FILTERS BELOW
+function filterResults(){
+  const allBooksApiUrl = "https://localhost:7258/api/books";
+  fetch(allBooksApiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      json.forEach((book) => {
+        masterBookList.push(book);
+      });
+      console.log(masterBookList)
+      updateResult("")
+    });
+
+const updateResult = query => {
+let resultList = document.querySelector(".result");
+resultList.innerHTML = "";
+
+masterBookList.map(algo =>{
+        console.log(algo.title)
+  query.split(" ").map(word =>{
+    if(algo.title.toLowerCase().indexOf(word.toLowerCase()) != -1){
+      resultList.innerHTML += `<li id = "${algo.isn}"class="list-group-item" onclick = "onBookClick(this.id)">${algo.title}</li>`;
+    }
+  })
+})
+}
+}
+
+function filterCondition(){
+  const allBooksApiUrl = "https://localhost:7258/api/books";
+  fetch(allBooksApiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      json.forEach((book) => {
+        masterBookList.push(book);
+      });
+      console.log(masterBookList)
+      updateResult("")
+    });
+
+const updateResult = query => {
+let resultList = document.querySelector(".result");
+resultList.innerHTML = "";
+
+masterBookList.map(algo =>{
+        console.log(algo.condition)
+  query.split(" ").map(word =>{
+    if(algo.title.toLowerCase().indexOf(word.toLowerCase()) != -1){
+      resultList.innerHTML += `<li id = "${algo.isn}"class="list-group-item" onclick = "onBookClick(this.id)">${algo.title} -- ${algo.condition}</li>`;
+    }
+  })
+})
+}
+}
+
+function filterAuthor(){
+  const allBooksApiUrl = "https://localhost:7258/api/books";
+  fetch(allBooksApiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      json.forEach((book) => {
+        masterBookList.push(book);
+      });
+      console.log(masterBookList)
+      updateResult("")
+    });
+
+const updateResult = query => {
+let resultList = document.querySelector(".result");
+resultList.innerHTML = "";
+
+masterBookList.map(algo =>{
+        console.log(algo.author)
+  query.split(" ").map(word =>{
+    if(algo.title.toLowerCase().indexOf(word.toLowerCase()) != -1){
+      resultList.innerHTML += `<li id = "${algo.isn}"class="list-group-item" onclick = "onBookClick(this.id)">${algo.title} -- ${algo.author}</li>`;
+    }
+  })
+})
+}
+}
+
